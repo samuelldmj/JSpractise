a5bd4be51415ed2d534c8e117e684a4c7a5b131f
samuelldmj <samuelldmj5@gmail.com>
2025-02-06 22:49:59 +0100

    basic crud with fetch api. using wait and async and promise

--- fetchRestApi/fetchRest.js
+++ fetchRestApi/fetchRest.js
@@ -1,19 +1,141 @@
+// import { FetchHttp } from "./requestHandler/fetchHttp.js";
+
+// let serverUrl = `http://127.0.0.1:5000/api`;
+
+
+//GET OPERATION
+// let getBtn = document.getElementById('get-btn');
+// getBtn.addEventListener('click', function(){
+//    fetchEmployees();
+// })
+
+//display all employees
+// let fetchEmployees = function(){
+//     let url = `${serverUrl}/employees`;
+
+//     FetchHttp.get(url).then( employees => {
+//      console.log(employees);
+//      let tableBody = document.querySelector('#table-body');
+//      // Clear previous entries before adding new ones
+//      tableBody.innerHTML = '';
+
+//      for (const employee of employees) {
+//        let html = `
+//             <tr>
+//             <td>${employee.id}</td>
+//             <td>${employee.first_name}</td>
+//             <td>${employee.last_name}</td>
+//             <td>${employee.email}</td>
+//             <td>${employee.gender}</td>
+//             <td>${employee.ip_address}</td>
+//             </tr>
+//        `    
+//        tableBody.insertAdjacentHTML('beforeend', html);  
+//      }
+
+     
+//     }).catch(err => {
+//      console.error(err);
+//     })
+// }
+
+
+//POST operations
+
+// let postBtn = document.getElementById('post-btn');
+
+// postBtn.addEventListener('click', function(){
+//         let url = `${serverUrl}/employees`;
+    
+//         let employeeData = {
+//             first_name: 'bola',
+//             last_name: 'Ade',
+//             email: 'bolade@hotmail.com',
+//             ip_address: '127.0.0.4', // Corrected IP format
+//             gender: 'male'
+//         };
+
+//         FetchHttp.post(url, employeeData).then(jsonResponse => {
+
+//             console.log(jsonResponse);
+//             fetchEmployees();
+//         }).catch(err => {
+//      console.error(err);
+//     })
+// })
+
+
+//PUT OPERATION
+// let putBtn = document.getElementById('put-btn');
+
+// putBtn.addEventListener('click', function(){
+
+//     let emID = '_0jwqbx4';
+//     let url = `${serverUrl}/employees/${emID}`;
+
+//     let updateEmployee = {
+//         id : emID,
+//         first_name : 'Samuel',
+//         last_name: 'bolu',
+//         gender : 'male',
+//         email : 'samb@hotmail.com',
+//         ip_address : '127.0.0.1'
+//     }
+
+//     FetchHttp.put(url, updateEmployee).then(jsonResponse => {
+//         console.log(jsonResponse);
+//         fetchEmployees()
+//     })
+//     .catch(err => {
+//         console.error(err)
+//     })
+// })
+
+//DELETE OPERATION
+// let deleteBtn = document.querySelector('#delete-btn');
+
+// deleteBtn.addEventListener('click', function(){
+//     let emID = '_e03lls2';
+//     let url = `${serverUrl}/employees/${emID}`;
+
+//     FetchHttp.delete(url).then(jsonResponse => {
+//             console.log(jsonResponse);
+//             console.log('Hello');
+//             fetchEmployees();
+//     }).catch(err => {
+//         console.error(err);
+//     })
+// })
+
+
+
+
+/*
+======================================================================================================
+                    USING ASYNC AND AWAIT
+======================================================================================================
+
+ */
+
 import { FetchHttp } from "./requestHandler/fetchHttp.js";
 
 let serverUrl = `http://127.0.0.1:5000/api`;
-//GET OPERATION
+
+
+// GET request
+
 let getBtn = document.getElementById('get-btn');
 getBtn.addEventListener('click', function(){
    fetchEmployees();
 })
 
-//display all employees
-let fetchEmployees = function(){
-    let url = `${serverUrl}/employees`;
 
-    FetchHttp.get(url).then( employees => {
-     console.log(employees);
-     let tableBody = document.querySelector('#table-body');
+async function fetchEmployees() {
+    try {
+        let url = `${serverUrl}/employees`;
+        const employees = await FetchHttp.get(url);
+
+        let tableBody = document.querySelector('#table-body');
      // Clear previous entries before adding new ones
      tableBody.innerHTML = '';
 
@@ -30,47 +152,57 @@ let fetchEmployees = function(){
        `    
        tableBody.insertAdjacentHTML('beforeend', html);  
      }
-
-     
-    }).catch(err => {
-     console.error(err);
-    })
+        
+    } catch (error) {
+        console.error('Failed to fetch data:', error);
+    }
 }
 
 
-//POST operations
 
+// POST request
 let postBtn = document.getElementById('post-btn');
 
 postBtn.addEventListener('click', function(){
-        let url = `${serverUrl}/employees`;
-    
+    createEmployee();
+    fetchEmployees();
+
+})
+
+async  function createEmployee(){
+    let url = `${serverUrl}/employees`;
+    try{
         let employeeData = {
             first_name: 'bola',
             last_name: 'Ade',
             email: 'bolade@hotmail.com',
-            ip_address: '127.0.0.4', // Corrected IP format
+            ip_address: '127.0.0.4',
             gender: 'male'
         };
 
-        FetchHttp.post(url, employeeData).then(jsonResponse => {
+        let createEmployee = await FetchHttp.post(url, employeeData)
+            console.log(createEmployee);
+            return createEmployee;
+        }catch(err ) {
+         console.error(err);
+        }
+}
 
-            console.log(jsonResponse);
-            fetchEmployees();
-        }).catch(err => {
-     console.error(err);
-    })
-})
 
-
-//PUT OPERATION
+// PuT request
 let putBtn = document.getElementById('put-btn');
 
 putBtn.addEventListener('click', function(){
+    updateEmployee();
+    fetchEmployees();
 
-    let emID = '_0jwqbx4';
+})
+
+async  function updateEmployee(){
+    let emID = '_dyf79y5' ;
     let url = `${serverUrl}/employees/${emID}`;
-
+    try{
+      
     let updateEmployee = {
         id : emID,
         first_name : 'Samuel',
@@ -80,27 +212,34 @@ putBtn.addEventListener('click', function(){
         ip_address : '127.0.0.1'
     }
 
-    FetchHttp.put(url, updateEmployee).then(jsonResponse => {
-        console.log(jsonResponse);
-        fetchEmployees()
-    })
-    .catch(err => {
-        console.error(err)
-    })
-})
+        let createEmployee = await FetchHttp.put(url, updateEmployee)
+            console.log(createEmployee);
+            return createEmployee;
+        }catch(err ) {
+         console.error(err);
+        }
+}
 
-//DELETE OPERATION
+
+
+// delete request
 let deleteBtn = document.querySelector('#delete-btn');
 
 deleteBtn.addEventListener('click', function(){
-    let emID = '_e03lls2';
-    let url = `${serverUrl}/employees/${emID}`;
-
-    FetchHttp.delete(url).then(jsonResponse => {
-            console.log(jsonResponse);
-            console.log('Hello');
-            fetchEmployees();
-    }).catch(err => {
-        console.error(err);
-    })
+    deleteEmployee();
+    fetchEmployees();
 })
+
+
+async function deleteEmployee(){
+    try {
+     let emID = '_7v564dr' ;
+     let url = `${serverUrl}/employees/${emID}`;
+
+     let jsonResponse = await FetchHttp.delete(url);
+     console.log(jsonResponse);
+     return jsonResponse;
+    } catch (error) {
+        console.error(error);
+    }
+}
--- fetchRestApi/requestHandler/fetchHttp.js
+++ fetchRestApi/requestHandler/fetchHttp.js
@@ -1,72 +1,154 @@
+// export class FetchHttp {
+
+//      constructor(){
+//             //this.http = fetch();
+//      }
+
+
+//      static get(url){
+
+//         return new Promise((resolve, reject) => {
+//             fetch(url).then(response => {
+//                 return response.json()
+//             }).then(data => {
+//                 resolve(data);
+//             }).catch(err => {
+//                 reject(err);
+//             })
+//         })
+//      }
+
+//      static post(url, data){
+//         return new Promise((resolve, reject) => {
+//             fetch(url, {
+//                method : 'POST',
+//                headers : {
+//                 'Content-type' : 'application/json'
+//                },
+//                body : JSON.stringify(data)
+//             }).then(response => {
+//                return response.json();
+//             }).then(data => {
+//                 resolve(data);
+//             }).catch(err => {
+//                 reject(err)
+//             })
+//         })
+//      }
+
+
+
+//      static put(url, data){
+//         return new Promise((resolve, reject) => {
+//             fetch(url, {
+//                 method : 'PUT',
+//                 headers : {
+//                  'Content-type' : 'application/json'
+//                 },
+//                 body : JSON.stringify(data)
+//              }).then(response => {
+//                 return response.json()
+//              }).then(data => {
+//                  resolve(data);
+//              }).catch(err => {
+//                  reject(err)
+//              })
+//          })
+//      }
+
+//      static delete(url){
+//         return new Promise((resolve, reject) => {
+//             fetch(url, {
+//                 method : 'DELETE',
+//              }).then(response => {
+//                 return response.json()
+//              }).then(data => {
+//                  resolve(data);
+//              }).catch(err => {
+//                  reject(err)
+//              })
+//          })
+//      }
+// }
+
+
+
+
+/*
+======================================================================================================
+                    USING ASYNC AND AWAIT
+======================================================================================================
+
+ */
+
+
 export class FetchHttp {
 
-     constructor(){
-            //this.http = fetch();
-     }
+
+    static async get(url) {
+        try {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            FetchHttp.handleError(error)
+        }
+    }
 
 
-     static get(url){
-
-        return new Promise((resolve, reject) => {
-            fetch(url).then(response => {
-                return response.json()
-            }).then(data => {
-                resolve(data);
-            }).catch(err => {
-                reject(err);
-            })
-        })
-     }
-
-     static post(url, data){
-        return new Promise((resolve, reject) => {
-            fetch(url, {
-               method : 'POST',
-               headers : {
-                'Content-type' : 'application/json'
-               },
-               body : JSON.stringify(data)
-            }).then(response => {
-               return response.json();
-            }).then(data => {
-                resolve(data);
-            }).catch(err => {
-                reject(err)
-            })
-        })
-     }
-
-
-
-     static put(url, data){
-        return new Promise((resolve, reject) => {
-            fetch(url, {
-                method : 'PUT',
-                headers : {
-                 'Content-type' : 'application/json'
+    static async post(url, data) {
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
                 },
-                body : JSON.stringify(data)
-             }).then(response => {
-                return response.json()
-             }).then(data => {
-                 resolve(data);
-             }).catch(err => {
-                 reject(err)
-             })
-         })
-     }
+                body: JSON.stringify(data)
+            })
 
-     static delete(url){
-        return new Promise((resolve, reject) => {
-            fetch(url, {
-                method : 'DELETE',
-             }).then(response => {
-                return response.json()
-             }).then(data => {
-                 resolve(data);
-             }).catch(err => {
-                 reject(err)
-             })
-         })
-     }
+            const jsonResponse = await response.json();
+            return jsonResponse;
+        } catch (error) {
+            FetchHttp.handleError(error)
+        }
+    }
+
+    static async put(url, data) {
+        try {
+            const response = await fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+
+            const jsonResponse = await response.json();
+            return jsonResponse;
+        } catch (error) {
+
+            FetchHttp.handleError(error)
+        }
+    }
+
+    static async delete(url) {
+        try {
+            const response = await fetch(url, {
+                method: 'DELETE',
+            });
+            if (!response.ok) { // Check if response status is not in the range 200-299
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            // If you expect a JSON response, then parse it, otherwise, you might just return the response or a success message
+            return await response.json(); // This line might be unnecessary unless you're expecting a response body
+        } catch (error) {
+            FetchHttp.handleError(error)
+        }
+    }
+
+    static async handleError(error) {
+        console.error('Fetch error:', error);
+        throw error;
+    }
+
 }
